9aa7a8be8e24ae2d929a72637d0621cbe0a981d5
Evan Cameron <cameron.evan@gmail.com>
2021-12-06 10:41:52 -0500

    an easy one today

---
+++ src/day6.rs
@@ -0,0 +1,44 @@
+use anyhow::{Context, Result};
+
+fn input(s: &'static str) -> [u64; 9] {
+    let mut pop = [0; 9];
+    // not sure why I can't remove the trailing \n but whatever
+    for n in s.trim_end().split(',').map(|n| n.parse::<usize>().unwrap()) {
+        pop[n] += 1;
+    }
+    pop
+}
+
+fn part_one(mut fish: [u64; 9], days: usize) -> u64 {
+    for _ in 0..days {
+        fish.rotate_left(1);
+        fish[6] += fish[8];
+    }
+    fish.iter().sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn day6_1_test() {
+        assert_eq!(5_934, dbg!(part_one(input("3,4,3,1,2"), 80)));
+    }
+
+    #[test]
+    fn day6_1() {
+        assert_eq!(
+            374_927,
+            dbg!(part_one(input(include_str!("../data/day6_1.txt")), 80))
+        );
+    }
+
+    #[test]
+    fn day6_2() {
+        assert_eq!(
+            1_687_617_803_407,
+            dbg!(part_one(input(include_str!("../data/day6_1.txt")), 256))
+        );
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod day2;
 mod day3;
 mod day4;
 mod day5;
+mod day6;
 
 fn main() {
     day4::part_two().unwrap();
